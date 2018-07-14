e0ddcf8b9d617f7880c006adb34b9e60712d4011
Pawel <pawelurbanik94@gmail.com>
2018-07-14 13:30:47 +0200

    Some logic changes

--- app/src/main/java/com/example/pawel/pharse/MainActivity.java
+++ app/src/main/java/com/example/pawel/pharse/MainActivity.java
@@ -14,11 +14,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String initialText;
+    /* Views*/
+    private Button copyButton;
+    private Button findButton;
+    private Button resetButton;
     private TextView textView;
+    private TextView messageTextView;
+
+
+    /*Variables*/
+    private String initialText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +35,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         /* Views */
-        Button copyButton = (Button) findViewById(R.id.copyButton);
-        final Button findButton = (Button) findViewById(R.id.findPhrase);
-        Button resetButton = (Button) findViewById(R.id.resetButton);
+        copyButton = (Button) findViewById(R.id.copyButton);
+        findButton = (Button) findViewById(R.id.findPhrase);
+        resetButton = (Button) findViewById(R.id.resetButton);
         textView = (TextView) findViewById(R.id.textView);
-        final TextView messageTextView = (TextView) findViewById(R.id.message);
+        messageTextView = (TextView) findViewById(R.id.message);
 
         /*Variables */
         String phrase;
@@ -40,13 +49,21 @@ public class MainActivity extends AppCompatActivity {
         copyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
                 ClipboardManager clipboardManager = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
                 String pasteData= "";
                 ClipData.Item item = clipboardManager.getPrimaryClip().getItemAt(0);
                 pasteData = item.getText().toString();
-                textView.setText(pasteData);
-                messageTextView.setText("Click find pfrase");
-                initialText= textView.getText().toString();
+                if (pasteData != "") {
+                    textView.setText(pasteData);
+                    messageTextView.setText("Click find pfrase");
+                    initialText = textView.getText().toString();
+                }else
+                {
+                    messageTextView.setText("Clipboard is empty");
+                    Toast.makeText(getApplicationContext(), "Clipboard is empty", Toast.LENGTH_SHORT);
+                }
 
             }
         });
@@ -54,26 +71,32 @@ public class MainActivity extends AppCompatActivity {
         findButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("What find?");
-                final EditText findPhrase = new EditText(MainActivity.this);
-                builder.setView(findPhrase);
-                builder.setPositiveButton("Find", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                    String phrase  = findPhrase.getText().toString();
-                    if (!phrase.equals(""))
-                    {
-                        messageTextView.setText(findPhrase(phrase));
-                    }
-                    else {
-                        textView.setText(initialText);
-                        messageTextView.setText("No phrase selected");
-                    }
+                if (textView.getText().toString().isEmpty())
+                {
+                    messageTextView.setText("Text is empty");
+                    Toast.makeText(MainActivity.this, "Text is empty", Toast.LENGTH_SHORT).show();
 
-                    }
-                });
-                builder.show();
+                }
+                else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setTitle("What find?");
+                    final EditText findPhrase = new EditText(MainActivity.this);
+                    builder.setView(findPhrase);
+                    builder.setPositiveButton("Find", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            String phrase = findPhrase.getText().toString();
+                            if (!phrase.equals("")) {
+                                messageTextView.setText(findPhrase(phrase));
+                            } else {
+                                textView.setText(initialText);
+                                messageTextView.setText("No phrase selected");
+                            }
+
+                        }
+                    });
+                    builder.show();
+                }
             }
         });
 
@@ -81,6 +104,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 messageTextView.setText("Copy text from clipboard");
+                textView.setText("");
                 textView.setHint("Copy text from clipboard");
             }
         });
